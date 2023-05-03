aee076626f92463f7de2e8d4175f49de2dba15bd
Melly <78555659+M3llySlayz@users.noreply.github.com>
2023-05-02 21:32:22 -0400

    Song Names in the middle??? (Freeplay)

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -114,6 +114,8 @@ class FreeplayState extends MusicBeatState
 			var songText:Alphabet = new Alphabet(0, (70 * i) + 30, songs[i].songName, true, false);
 			songText.isMenuItem = true;
 			songText.targetY = i;
+                        songText.screenCenter(X);
+                        songText.changeX = false; // pretty sure its wrong but whatever
 			grpSongs.add(songText);
 
 			if (songText.width > 980)
